89263a78fb560d2c863dd54a1c3776863260be4e
Guilherme Matos <guimvmatos1@outlook.com>
2020-11-08 00:31:26 -0300

    Update gen_testdata.py
    correction

--- testdata/gen_testdata.py
+++ testdata/gen_testdata.py
@@ -107,12 +107,12 @@ pktCnt = 0
 
 def pkt_send():
     global pktCnt
-    pkt = Ether(src=MAC2, dst=MAC2) / IPv6(src="fc00::1",dst="fc00::2") / UDP (sport=64515, dport=2152 ) / GTP_U_Header(TEID=32, Reserved=0) / dl_pdu_session(gtp_ext=133,QoSID=14,E=1) / IPv6(dst="fc00::2" , src="fc00::1") / UDP(dport=80,sport=35000)
+    pkt = Ether(src=MAC2, dst=MAC2) / IPv6(src="fc00::1",dst="fc00::2") / UDP (sport=64515, dport=2152 ) / GTP_U_Header(TEID=32, Reserved=0, E=1) / dl_pdu_session(gtp_ext=133,QoSID=14) / IPv6(dst="fc00::2" , src="fc00::1") / UDP(dport=80,sport=35000)
     pkt.show2()
     pkt = pad_pkt(pkt, 64)
     applyPkt(pkt, 'nf0', pktCnt)
     pktCnt += 1
-    pkt = Ether(src=MAC2, dst=MAC2) / IPv6(src="fc00::1",dst="fc00::2") / UDP (sport=64515, dport=2152 ) / GTP_U_Header(TEID=32, Reserved=0) / dl_pdu_session(gtp_ext=133,QoSID=14,E=1) / IPv6(dst="fc00::2" , src="fc00::1") / UDP(dport=80,sport=35000)
+    pkt = Ether(src=MAC2, dst=MAC2) / IPv6(src="fc00::1",dst="fc00::2") / UDP (sport=64515, dport=2152 ) / GTP_U_Header(TEID=32, Reserved=0, E=1) / dl_pdu_session(gtp_ext=133,QoSID=14) / IPv6(dst="fc00::2" , src="fc00::1") / UDP(dport=80,sport=35000)
     pkt.show2()
     pkt = pad_pkt(pkt, 64)
     expPkt(pkt, 'nf1')
